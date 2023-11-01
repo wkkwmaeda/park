df8deaa40c1368cbf2d272ca95b89bb053ece73c
T21018-PC <T21018-PC@T21018-PC>
2023-11-01 15:59:44 +0900

    表示

--- Park2/src/main/java/servlet/ParkingStatusServlet.java
+++ Park2/src/main/java/servlet/ParkingStatusServlet.java
@@ -1,38 +1,35 @@
 package servlet;
-import java.io.IOException;
-import java.util.List;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.ParkingDatabaseUtil;
-import model.ParkingSpace;
+import dao.ParkingDAO;
 
-@WebServlet("/ParkingStatusServlet")
 public class ParkingStatusServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // 駐車場の空き状況を取得するロジックを実行
-    	
-    	System.out.println("データベースからデータを取得します。");
-    	List<ParkingSpace> parkingSpace = ParkingDatabaseUtil.getParkingStatus();
-    	System.out.println("取得したデータ数: " + parkingSpace.size());
-    	
-    	for (ParkingSpace space : parkingSpace) {
-    	    System.out.println("駐車場番号: " + space.getParkingNumber() + ", 状態: " + space.getStatus());
-    	}
     
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
+        // フォームから送信されたデータを取得
+        String customerID = request.getParameter("customerId");
+        String carNumber = request.getParameter("carNumber");
 
-        // 結果をリクエスト属性に設定
-        request.setAttribute("parkingSpace", parkingSpace);
+        // ユーザー認証を行う
+        String[] userInfo = ParkingDAO.authenticateUser(customerID, carNumber);
 
-        // JSP ページにフォワード
-        request.getRequestDispatcher("/WEB-INF/jsp/park.jsp").forward(request, response);
+        if (userInfo != null) {
+            // ユーザーが認証された場合の処理
+            out.println("<h1>Welcome, " + userInfo[0] + "!</h1>");
+            out.println("<p>Your car number: " + userInfo[1] + "</p>");
+            // 他の処理を追加
+        } else {
+            // 認証失敗時の処理
+            out.println("<h1>Authentication failed. Please check your credentials.</h1>");
+        }
     }
 }
