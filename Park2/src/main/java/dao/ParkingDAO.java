38152819a69ed826d0510bbbfe011f9128aa0fdf
T21018-PC <T21018-PC@T21018-PC>
2023-11-01 15:51:37 +0900

    配列を返却するように変更

--- Park2/src/main/java/dao/ParkingDAO.java
+++ Park2/src/main/java/dao/ParkingDAO.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ParkingDAO {
-	// データベースへの接続情報
+    // データベースへの接続情報
     private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
     private static final String JDBC_URL = "jdbc:mysql://localhost:65534/parking?useSSL=false&characterEncoding=UTF-8&serverTimezone=JST";
     private static final String DB_USER = "root";
@@ -22,16 +22,17 @@ public class ParkingDAO {
     }
 
     // ユーザーの認証を行うメソッド
-    public static boolean authenticateUser(String customerID, String carNumber) {
+    public static String[] authenticateUser(String customerID, String carNumber) {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
+        String[] userInfo = new String[2]; // ユーザー情報を保持する配列
 
         try {
             conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
 
             // データベースに対するクエリ作成
-            String sql = "SELECT * FROM customer WHERE cuid = ? AND carnum = ?";
+            String sql = "SELECT cuid, carnum FROM customer WHERE cuid = ? AND carnum = ?";
             stmt = conn.prepareStatement(sql);
             stmt.setString(1, customerID);
             stmt.setString(2, carNumber);
@@ -39,7 +40,10 @@ public class ParkingDAO {
             rs = stmt.executeQuery();
 
             if (rs.next()) {
-                return true; // ユーザーが見つかった場合はtrueを返す
+                // ユーザーが見つかった場合はユーザー情報を配列に格納して返す
+                userInfo[0] = rs.getString("cuid"); // customerIdを取得してuserInfo[0]に格納
+                userInfo[1] = rs.getString("carnum"); // carNumberを取得してuserInfo[1]に格納
+                return userInfo;
             }
         } catch (SQLException e) {
             e.printStackTrace(); // エラーを出力
@@ -53,6 +57,6 @@ public class ParkingDAO {
             }
         }
 
-        return false; // ユーザーが見つからなかった場合はfalseを返す
+        return null; // ユーザーが見つからなかった場合はnullを返す
     }
 }
