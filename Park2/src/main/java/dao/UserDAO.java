195387dc05e1cd591f5f8efb44a2e2daaf1a7218
T21018-PC <T21018-PC@T21018-PC>
2023-11-01 14:58:55 +0900

    ログインから作りましょう

--- Park2/src/main/java/dao/UserDAO.java
+++ Park2/src/main/java/dao/UserDAO.java
@@ -7,55 +7,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
-    // 他のメソッドとフィールド
-	
-	private final static String url = "jdbc:mysql://localhost:65534/park?" + 
-			"allowPublicKeyRetrieval=true&useSSL=false&characterEncoding=UTF-8&serverTimezone=JST";
 
-	private final static String dbUser = "root";
-	private final static String dbPassword = "";
-    
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:65534/parking?useSSL=false&characterEncoding=UTF-8&serverTimezone=JST";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "pass"; 
+
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(DRIVER_NAME);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("JDBCドライバーの読み込みに失敗しました。", e);
         }
     }
 
-
     public static boolean authenticateUser(String userID, String password) {
-        // データベース接続情報（適切に設定してください）
-        
-        // データベース接続オブジェクト
         Connection conn = null;
-        // SQLステートメント
         PreparedStatement stmt = null;
-        // 結果セット
         ResultSet rs = null;
 
         try {
-            // データベースに接続
-            conn = DriverManager.getConnection(url, dbUser, dbPassword);
+            conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
 
-            // SQLクエリを作成（テーブル名、カラム名は適切に変更してください）
             String sql = "SELECT * FROM user WHERE user_id = ? AND password = ?";
             stmt = conn.prepareStatement(sql);
             stmt.setString(1, userID);
-            stmt.setString(2, password);
+            stmt.setString(2, password); // ハッシュ化することが推奨されます
 
-            // クエリを実行
             rs = stmt.executeQuery();
 
-            // 結果が存在する場合は、認証成功とみなす
             if (rs.next()) {
                 return true;
             }
         } catch (SQLException e) {
-            // エラーハンドリング
-            e.printStackTrace();
+            e.printStackTrace(); // 適切なエラーハンドリングを追加することを検討してください
         } finally {
-            // リソースのクローズ
             try {
                 if (rs != null) rs.close();
                 if (stmt != null) stmt.close();
@@ -65,6 +51,6 @@ public class UserDAO {
             }
         }
 
-        return false; // 認証失敗
+        return false;
     }
 }
