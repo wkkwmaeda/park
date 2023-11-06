2b346c64f8662941f9c05fa5b3e1b2d57f87dcef
T21018-PC <T21018-PC@T21018-PC>
2023-11-06 13:53:54 +0900

    一覧

--- Park2/src/main/java/servlet/ParkingStatusServlet.java
+++ Park2/src/main/java/servlet/ParkingStatusServlet.java
@@ -2,6 +2,8 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +14,7 @@ import dao.ParkingDAO;
 public class ParkingStatusServlet extends HttpServlet {
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
+    	
         // フォームから送信されたデータを取得
         String customerID = request.getParameter("customerId");
         String carNumber = request.getParameter("carNumber");
@@ -23,13 +23,12 @@ public class ParkingStatusServlet extends HttpServlet {
         String[] userInfo = ParkingDAO.authenticateUser(customerID, carNumber);
 
         if (userInfo != null) {
-            // ユーザーが認証された場合の処理
-            out.println("<h1>Welcome, " + userInfo[0] + "!</h1>");
-            out.println("<p>Your car number: " + userInfo[1] + "</p>");
-            // 他の処理を追加
+        	//ログイン結果画面にフォワード
+    		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/Edit.jsp");
+    		dispatcher.forward(request, response);
         } else {
-            // 認証失敗時の処理
-            out.println("<h1>Authentication failed. Please check your credentials.</h1>");
+        	// ログイン失敗
+            request.getRequestDispatcher("WEB-INF/jsp/error.jsp").forward(request, response);
         }
     }
 }
