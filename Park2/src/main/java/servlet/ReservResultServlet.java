5c31123e17537fdf1db7b127e097bd71e182236e
畠山翔２ <@T21016-PC>
2023-11-16 18:51:37 +0900

    hello

--- Park2/src/main/java/servlet/ReservResultServlet.java
+++ Park2/src/main/java/servlet/ReservResultServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ParkingDAO;
+
 @WebServlet("/ReservResultServlet")
 public class ReservResultServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -15,21 +17,35 @@ public class ReservResultServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Retrieve form data
-    	String name = request.getParameter("name");
+        String cuname = request.getParameter("cuname");
         String tel = request.getParameter("tel");
         String carNumber = request.getParameter("carNumber");
         String checkInDate = request.getParameter("checkInDate");
         String checkOutDate = request.getParameter("checkOutDate");
 
         // Save data in session
-        request.getSession().setAttribute("name", name);
+        request.getSession().setAttribute("cuname", cuname);
         request.getSession().setAttribute("tel", tel);
         request.getSession().setAttribute("carNumber", carNumber);
         request.getSession().setAttribute("checkInDate", checkInDate);
         request.getSession().setAttribute("checkOutDate", checkOutDate);
 
-        // Forward to ReserctionResult.jsp
-        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/ReservtionResult.jsp");
-        dispatcher.forward(request, response);
+        // Create ParkingDAO instance
+        ParkingDAO parkingDAO = new ParkingDAO();
+
+        try {
+            // Attempt to create reservation
+            parkingDAO.createReservation(cuname, tel, carNumber, checkInDate, checkOutDate);
+
+            // Forward to ReserctionResult.jsp on success
+            RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/ReservtionResult.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace(); // Add proper logging
+
+            // Forward to error.jsp on failure
+            RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/error.jsp");
+            dispatcher.forward(request, response);
+        }
     }
 }
